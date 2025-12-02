9541df1758df220569b59a9ff15bfc2b8d47de44
InterPixel <theweeklyjubs@gmail.com>
2025-12-02 09:01:21 +0000

    Accepts Panasonic RAW

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const contact_sheet = document.querySelector('.contact-sheet');
 
 const file_input = document.createElement('input');
 file_input.type = 'file';
-file_input.accept = '.cr2';
+file_input.accept = '.rw2';
 file_input.multiple = true;
 upload_container.appendChild(file_input);
 
